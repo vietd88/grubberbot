c6093eb1671bbbea55712c6b2d6baa1b1d07b287
Paul Terwilliger <paul.terwil@gmail.com>
2021-09-15 09:57:34 -0400

    twitch tts

--- twitch/bot.py
+++ twitch/bot.py
@@ -1,11 +1,19 @@
 from twitchobserver import Observer
 import pyttsx3
+from dotenv import load_dotenv
+import os
 
 # for the tts
+load_dotenv()
+TWITCH_TOKEN = os.getenv('TWITCH_TOKEN')
 engine = pyttsx3.init()
+engine.setProperty('rate', 150)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-1].id)
+
 channel = "pawngrubber"
 
-with Observer("username", "Oauth") as observer:
+with Observer("username", TWITCH_TOKEN) as observer:
     observer.join_channel(channel)
     print("Joined channel", channel)
     print("Press Ctrl+C to quit, you might have to close the terminal window sometimes.")
@@ -17,7 +25,7 @@ with Observer("username", "Oauth") as observer:
                     message = event.message
 
                     # TODO get list of mods instead of hard-coding mod usernames
-                    mods = ["thefuxia", "smarterchess", "streamlabs", "grubberbot", "lalizig", "paradajzcity",
+                    mods = ['pawngrubber', "thefuxia", "smarterchess", "streamlabs", "grubberbot", "lalizig", "paradajzcity",
                     "duellinksguy", "thewretch2", "vietd", "poitaoforpresident", "chesscomchris", "mynameislegyon", "f0rgetaboutit",
                     "zuname", "rakshakthetall", "drittman13", "expired_febreeze", "strance_02", "marko_sreck", "flyingseverus"]
 
