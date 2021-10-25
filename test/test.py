1bea3050911aa85eca2243b4ab1582426e6b87f0
Paul Terwilliger <paul.terwil@gmail.com>
2021-10-25 07:12:31 -0400

    almost got docker-compose to work

--- test/test.py
+++ test/test.py
@@ -1,1 +1,27 @@
-print('hello test')
+import os
+import time
+from pprint import pformat, pprint
+import pprint
+import json
+
+JD = 'test.json'
+
+with open('test.txt', 'w') as f:
+    print(pformat(os.listdir()), file=f)
+
+counter = 0
+
+jd = []
+if os.path.exists(JD):
+    with open(JD, 'r') as f:
+        jd = json.load(f)
+
+while True:
+    time.sleep(1)
+    with open('test.txt', 'a') as f:
+        print(f'({counter}, {len(jd)})', file=f, end=', ')
+    counter += 1
+
+    jd.append(counter)
+    with open(JD, 'w') as f:
+        json.dump(jd, f)
