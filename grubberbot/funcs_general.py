e284bf1029f2ccfd060ac8596dfa6b1fda4e708a
Paul Terwilliger <paul.terwil@gmail.com>
2021-10-25 11:12:54 -0400

    Changed discord and google credentials, properly hid them this time

--- grubberbot/funcs_general.py
+++ grubberbot/funcs_general.py
@@ -3,7 +3,7 @@ import gspread
 import numpy as np
 import pandas as pd
 
-GOOGLE_TOKEN = "credentials/grubberbot-2f9a174696fa.json"
+GOOGLE_TOKEN = "credentials/google_credentials.json"
 GOOGLE_SHEET_NAME = "1SFH7ntDpDW7blX_xBU_m_kSmvY2tLXkuwq-TOM9wyew"
 
 REFRESH_MESSAGE = "Hi! Looks like "
